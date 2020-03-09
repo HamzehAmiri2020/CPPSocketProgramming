2c931f48350c49c673af56e18248f02b3069453e
HamzehAmiri2020 <HamzehAmiri2020@yahoo.com>
2020-03-09 14:14:42 +0330

    Test

---
+++ src/class1/BaseClass.h
@@ -0,0 +1,20 @@
+/*
+ * BaseClass.h
+ *
+ *  Created on: Feb 26, 2020
+ *      Author: ceph1
+ */
+
+#ifndef CLASS1_BASECLASS_H_
+#define CLASS1_BASECLASS_H_
+
+class BaseClass {
+public :
+	BaseClass();
+private:
+};
+
+
+
+
+#endif /* CLASS1_BASECLASS_H_ */
