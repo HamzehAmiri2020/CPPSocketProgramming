1458f8c50ced4987666f91be2c5a15ed72def2c6
ceph1 <ceph1@ceph1>
2020-03-10 06:04:51 +0330

    Init

--- src/MainRunner.cpp
+++ src/MainRunner.cpp
@@ -6,9 +6,11 @@
  */
 
 #include <iostream>
+#include "threadcpp/ProcessOne.h"
 
 int main(int argc, char **argv) {
-
+	ProcessOne p1;
+	p1.start();
 }
 
 
---
+++ src/threadcpp/ProcessOne.cpp
@@ -0,0 +1,20 @@
+/*
+ * ProcessOne.cpp
+ *
+ *  Created on: Mar 9, 2020
+ *      Author: ceph1
+ */
+
+#include "ProcessOne.h"
+
+ProcessOne::ProcessOne() {
+
+}
+
+ProcessOne::~ProcessOne() {
+}
+
+void ProcessOne::start() {
+
+}
+
