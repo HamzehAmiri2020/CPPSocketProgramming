29872cf3b0cb8ecb14fd64c055a3b3b8451e0bd0
ceph1 <ceph1@ceph1>
2020-03-14 03:07:22 +0330

    Socket

--- src/MainRunner.cpp
+++ src/MainRunner.cpp
@@ -10,6 +10,10 @@
 #include <iostream>
 #include <memory>
 #include "threadcpp/Thread.h"
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <unistd.h>
 
 using namespace std;
 
@@ -33,14 +37,58 @@ void* communicatingThread::run() {
 	return 0;
 }
 
-int main(int argc, char **argv) {
-	auto_ptr<communicatingThread> thread1(new communicatingThread(1));
-	auto_ptr<communicatingThread> thread2(new communicatingThread(2));
-	thread1->start();
-	thread2->start();
-	thread1->join();
-	thread2->join();
+#define PORT 8080
 
-	cout << "s = " << s << endl;
+int main(int argc, char **argv) {
+//	auto_ptr<communicatingThread> thread1(new communicatingThread(1));
+//	auto_ptr<communicatingThread> thread2(new communicatingThread(2));
+//	thread1->start();
+//	thread2->start();
+//	thread1->join();
+//	thread2->join();
+//
+//	cout << "s = " << s << endl;
+
+	int server_fd, new_socket, valread;
+	int opt = 1;
+	struct sockaddr_in address;
+	int addrlen = sizeof(address);
+	char buffer[1024] = { 0 };
+	char *hello = "Hello from server";
+
+	if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+		perror("socket failed");
+		exit(EXIT_FAILURE);
+	}
+
+	if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
+		perror("setsockopt");
+		exit(EXIT_FAILURE);
+	}
+
+	address.sin_family = AF_INET;
+	address.sin_addr.s_addr = INADDR_ANY;
+	address.sin_port = htons(PORT);
+
+// Forcefully attaching socket to the port 8080
+	if (bind(server_fd, (struct sockaddr*) &address, sizeof(address)) < 0) {
+		perror("bind failed");
+		exit(EXIT_FAILURE);
+	}
+
+	if (listen(server_fd, 3) < 0) {
+		perror("listen");
+		exit(EXIT_FAILURE);
+	}
+
+	if ((new_socket = accept(server_fd, (struct sockaddr*) &address, (socklen_t*) &addrlen)) < 0) {
+		perror("accept");
+		exit(EXIT_FAILURE);
+	}
+	valread = read(new_socket, buffer, 1024);
+	printf("%s\n", buffer);
+	send(new_socket, hello, strlen(hello), 0);
+	printf("Hello message sent\n");
+	return 0;
 }
 
---
+++ src/socket/MySocket.h
@@ -0,0 +1,19 @@
+/*
+ * MySocket.h
+ *
+ *  Created on: Mar 11, 2020
+ *      Author: ceph1
+ */
+
+#ifndef SOCKET_MYSOCKET_H_
+#define SOCKET_MYSOCKET_H_
+
+class MySocket {
+private:
+
+public:
+};
+
+
+
+#endif /* SOCKET_MYSOCKET_H_ */
